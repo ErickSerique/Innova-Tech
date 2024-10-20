5c216e8f215950d286fbc630ce1a5e61170f478d
Daniel Angelus Gai Silva <danielangelusgai19@gmail.com>
2024-10-20 18:05:48 -0300

    logica de login e cadastro
    adicionei o login e o cadastro junto com o carlos

---
+++ siteinnova/site/processos_db/innova_db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS innova_db;
+USE innova_db;
+
+CREATE TABLE IF NOT EXISTS usuario(
+nome VARCHAR(60) NOT NULL,
+email VARCHAR(45) NOT NULL,
+senha VARCHAR(65) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS mensagem_suporte(
+nome VARCHAR(60) NOT NULL,
+email varchar(45) NOT NULL,
+telefone INT(11),
+empresa VARCHAR(60),
+mensagem VARCHAR(600));

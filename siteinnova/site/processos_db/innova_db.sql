0ae1ba8c8abf3e9d0e93dc2d1ea0ab25dad3c787
Daniel Angelus Gai Silva <danielangelusgai19@gmail.com>
2024-12-01 19:33:33 -0300

    tirando os botões de cadastro e login

--- siteinnova/site/processos_db/innova_db.sql
+++ siteinnova/site/processos_db/innova_db.sql
@@ -1,17 +1,6 @@
 CREATE DATABASE IF NOT EXISTS innova_db;
 USE innova_db;
 
-CREATE TABLE usuario (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user',
-    status ENUM('active', 'inactive') DEFAULT 'active',
-    ultimo_login TIMESTAMP NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
 
 CREATE TABLE IF NOT EXISTS mensagem_suporte(
 nome VARCHAR(60) NOT NULL,

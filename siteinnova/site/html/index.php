238a4d9de7eb18c212d78b6fe1e511792d05e30b
Rafael Marques <rafaeltbm@gmail.com>
2024-12-03 14:09:16 -0300

    Update index.php

--- siteinnova/site/html/index.php
+++ siteinnova/site/html/index.php
@@ -126,11 +126,11 @@ $nomeUsuario = isset($_SESSION['nome']) ? htmlspecialchars($_SESSION['nome']) : 
       <?php if ($nomeUsuario): ?>
         <!-- Botão personalizado com o nome do usuário -->
         <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn btn-dark1 dropdown-toggle" type="button10" data-bs-toggle="dropdown" aria-expanded="false">
             <?=$nomeUsuario?>
           </button>
-          <ul class="dropdown-menu">
-            <li><a href="./logout.php"><button class="dropdown-item" type="button"></a>SAIR</button></a></li>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a href="./logout.php"><button class="dropdown-item" type="button10"></a>SAIR</button></a></li>
           </ul>
         </div>
 
